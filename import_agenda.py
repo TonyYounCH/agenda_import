2d454ac0b8e8d2d6cf8bc26463f8dbed11f32b08
TonyYounCH <tonyyoun2@gmail.com>
2021-03-22 16:35:36 -0700

    table attribute names updated to follow instruction

--- import_agenda.py
+++ import_agenda.py
@@ -8,18 +8,18 @@ from db_table import db_table
 class import_agenda:
 	def __init__(self, data):
 		self.data = data
-		self.length = len(self.data["sessiont_date"])
+		self.length = len(self.data["date"])
 		self.sessions = db_table("sessions", 
 								{ 
 									"session_id": "integer PRIMARY KEY", 
-									"sessiont_date": "varchar(16) NOT NULL", 
+									"date": "varchar(16) NOT NULL", 
 									"time_start": "varchar(16) NOT NULL",
 									"time_end": "varchar(16) NOT NULL",
 									"session_type": "varchar(16) NOT NULL",
-									"session_title": "text NOT NULL",
-									"session_location": "text DEFAULT ''",
-									"session_description": "text DEFAULT ''",
-									"session_speakers": "text DEFAULT ''",
+									"title": "text NOT NULL",
+									"location": "text DEFAULT ''",
+									"description": "text DEFAULT ''",
+									"speaker": "text DEFAULT ''",
 									"subsessions": "text DEFAULT ''",
 								})
 
@@ -27,16 +27,16 @@ class import_agenda:
 		# Index for current excel file must start with 14
 		for i in range(14, self.length):
 			session_id = i
-			sessiont_date = self.data["sessiont_date"][i]
+			date = self.data["date"][i]
 			time_start = self.data["time_start"][i]
 			time_end = self.data["time_end"][i]
 			session_type = self.data["session_type"][i]
-			session_title = self.data["session_title"][i]
-			session_location = self.data["session_location"][i]
-			session_description = self.data["session_description"][i]
+			title = self.data["title"][i]
+			location = self.data["location"][i]
+			description = self.data["description"][i]
 			# session speakers are separated with " ; " instead of "; " in order to
 			# be selected by query with %LIKE%.
-			session_speakers = self.data["session_speakers"][i].replace("; ", " ; ")
+			speaker = self.data["speaker"][i].replace("; ", " ; ")
 			subsessions = ""
 
 			# subsessions attribute is "" when there exists no subsessions.
@@ -48,14 +48,14 @@ class import_agenda:
 			# insert into sessions table
 			self.sessions.insert({ 
 				"session_id": session_id, 
-				"sessiont_date": sessiont_date, 
+				"date": date, 
 				"time_start": time_start,
 				"time_end": time_end,
 				"session_type": session_type,
-				"session_title": session_title,
-				"session_location":session_location,
-				"session_description": session_description,
-				"session_speakers": session_speakers,
+				"title": title,
+				"location":location,
+				"description": description,
+				"speaker": speaker,
 				"subsessions": subsessions,
 			})
 
@@ -70,16 +70,19 @@ if __name__ == "__main__":
 	# get command line argument as filename
 	# Since current excel file does not have proper attribute names, the attribute
 	# names of each column need to be chaged with rename method.
+	if len(sys.argv) != 2:
+		sys.exit("You must specify a filename you want to import.")
+
 	excel_data_df = pd.read_excel(sys.argv[1], keep_default_na=False).rename(columns=
 		{
-		"Whova Agenda Excel Template": "sessiont_date", 
+		"Whova Agenda Excel Template": "date", 
 		"Unnamed: 1": "time_start",
 		"Unnamed: 2": "time_end",
 		"Unnamed: 3": "session_type",
-		"Unnamed: 4": "session_title",
-		"Unnamed: 5": "session_location",
-		"Unnamed: 6": "session_description",
-		"Unnamed: 7": "session_speakers",
+		"Unnamed: 4": "title",
+		"Unnamed: 5": "location",
+		"Unnamed: 6": "description",
+		"Unnamed: 7": "speaker",
 		})
 
 	data = excel_data_df.to_dict()
